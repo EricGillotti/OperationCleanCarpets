b9c39f9aebbf9fc072ce16760fa391fd974e5103
Eric Gillotti <eric.gillotti@att.net>
2021-07-28 21:45:50 -0400

    Starting organizing

--- occ_frontend/src/App.js
+++ occ_frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Visit our Facebook page!
-        </p>
+      <h6 className="App-header">
         <a
           className="App-link"
           href="https://www.facebook.com/Gparentisprofessionalcarpetcleaning/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit our Facebook page!
         </a>
-      </header>
+      </h6>
     </div>
   );
 }
