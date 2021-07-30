849c8887d923e754c84bba285b6624cd87b3e4f6
Eric Gillotti <eric.gillotti@att.net>
2021-07-29 21:37:07 -0400

    Started nav bar

--- occ_frontend/src/App.js
+++ occ_frontend/src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
+import React from "react"
+import './App.css'
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
-  return (
-    <div className="App">
-      <h6 className="App-header">
-        <a
-          className="App-link"
-          href="https://www.facebook.com/Gparentisprofessionalcarpetcleaning/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit our Facebook page!
-        </a>
-      </h6>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <h6 className="App-header">
+                <a
+                className="App-link"
+                href="https://www.facebook.com/Gparentisprofessionalcarpetcleaning/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Visit our Facebook page!
+                </a>
+            </h6>
+        </div>
+    );
 }
 
 export default App;
